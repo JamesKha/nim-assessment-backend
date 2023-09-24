a8d3092cba15dbae6a16fffc87959715cbf0752a
JamesKha <jimmy.kha@mytwu.ca>
2023-09-24 11:18:36 -0700

    Task 1a: Update menu item

--- controllers/menuController.js
+++ controllers/menuController.js
@@ -27,4 +27,38 @@ const create = async (req, res) => {
   }
 };
 
-module.exports = { getAll, getOne, create };
+const updateMenuItem = async (req, res) => {
+  try {
+    const menuItemId = req.params.id;
+    const menuItemData = req.body;
+    const existingMenuItem = await MenuItems.getOne(menuItemId);
+
+    if (!existingMenuItem) {
+      return res.status(404).send("Menu item not found");
+    }
+    // Code below updates name, price, description
+    if (menuItemData.name) {
+      existingMenuItem.name = menuItemData.name;
+    }
+    if (menuItemData.price) {
+      existingMenuItem.price = menuItemData.price;
+    }
+    if (menuItemData.description) {
+      existingMenuItem.description = menuItemData.description;
+    }
+
+    // Updates the last changed time
+    existingMenuItem.updatedAt = new Date();
+
+    // Saves the item after all changes
+    const updatedMenuItem = await existingMenuItem.save();
+
+    res.send(updatedMenuItem);
+    return null;
+  } catch (error) {
+    res.status(500).send(error);
+  }
+  return null;
+};
+
+module.exports = { getAll, getOne, create, updateMenuItem };
--- routes/menuRouter.js
+++ routes/menuRouter.js
@@ -4,6 +4,7 @@ const menuController = require("../controllers/menuController");
 const menuRouter = Router();
 
 menuRouter.get("/", menuController.getAll);
+menuRouter.put("/:id", menuController.updateMenuItem);
 menuRouter.get("/:id", menuController.getOne);
 menuRouter.post("/", menuController.create);
 
